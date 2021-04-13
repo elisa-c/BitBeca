97ea11d167eabe2adc96adcdf2348aae22184199
Renilson <renilson.moreiraf@gmail.com>
2021-04-13 18:03:31 -0300

    Criação do enum coresBitBeca

--- BitBeca/Controller/ViewController.swift
+++ BitBeca/Controller/ViewController.swift
@@ -7,10 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CoresBitBeca {
+    case corAbacate
+    case corBlack
+    var corSelecionada: UIColor {
+        switch self {
+        case .corAbacate: return  #colorLiteral(red: 0.5386368036, green: 0.5939192176, blue: 0.337885499, alpha: 1)
+        case .corBlack: return  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        }
+    }
+}
 
+class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cores(cor: .corBlack)
+    }
+
+    // MARK: - Cores
+    func cores(cor: CoresBitBeca) {
+        self.view.backgroundColor = cor.corSelecionada
     }
 }
