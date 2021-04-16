fa2d29a78d93cd6edd56599d2c110cc9ff9b0b41
Elisa Camillo <elisatcamillo@gmail.com>
2021-04-16 11:57:52 -0300

    resolvendo problema de path do viewcontroller

--- BitBeca/Controller/ViewController.swift
+++ BitBeca/Controller/ViewController.swift
@@ -1,35 +1,73 @@
-//
-//  ViewController.swift
-//  BitBeca
-//
-//  Created by Renilson Moreira Ferreira on 13/04/21.
-//
-
 import UIKit
+
 import Commons
+
 import Details
 
+
+
 enum CoresBitBeca {
-    case corAbacate
-    case corBlack
-    var corSelecionada: UIColor {
-        switch self {
-        case .corAbacate: return  #colorLiteral(red: 0.5386368036, green: 0.5939192176, blue: 0.337885499, alpha: 1)
-        case .corBlack: return  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        }
-    }
+
+case corAbacate
+
+case corBlack
+
+var corSelecionada: UIColor {
+
+switch self {
+
+case .corAbacate: return #colorLiteral(red: 0.5386368036, green: 0.5939192176, blue: 0.337885499, alpha: 1)
+
+case .corBlack: return #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+
 }
 
-class ViewController: UIViewController {
-    let myProvider = CriptomoedaProvider()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        cores(cor: .corBlack)
-        myProvider.getData()
-    }
+}
 
-    // MARK: - Cores
-    func cores(cor: CoresBitBeca) {
-        self.view.backgroundColor = cor.corSelecionada
-    }
+}
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+@IBOutlet weak var tableBitcoins: UITableView!
+
+let myProvider = CriptomoedaProvider()
+
+override func viewDidLoad() {
+
+super.viewDidLoad()
+
+cores(cor: .corBlack)
+
+myProvider.getData()
+
+self.tableBitcoins.dataSource = self
+
+self.tableBitcoins.backgroundColor = .black
+
+}
+
+// MARK: - Cores
+
+func cores(cor: CoresBitBeca) {
+
+self.view.backgroundColor = cor.corSelecionada
+
+}
+
+// MARK: - TableView Tela Principal
+
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+return 20
+
+}
+
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+let cell = tableView.dequeueReusableCell(withIdentifier: "cellBitcoin", for: indexPath)
+
+return cell
+
+}
+
 }
