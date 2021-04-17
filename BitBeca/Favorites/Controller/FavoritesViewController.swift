9a0cef25c0f00e6ba2ac9e157a1fd1bd02b9332a
Renilson <renilson.moreiraf@gmail.com>
2021-04-17 01:39:26 -0300

    Melhorando estrutura da collection

--- BitBeca/Favorites/Controller/FavoritesViewController.swift
+++ BitBeca/Favorites/Controller/FavoritesViewController.swift
@@ -5,7 +5,7 @@
 
  import UIKit
 
- class FavoritesViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+ class FavoritesViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet weak var myCollectionView: UICollectionView!
     override func viewDidLoad() {
@@ -22,7 +22,15 @@
     // MARK: - Conteudo da linha
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cellFavorites = collectionView.dequeueReusableCell(withReuseIdentifier: "cellFavorites", for: indexPath) as? ListaFavoritesCollectionViewCell
+        cellFavorites?.labelNomeBitcoin.text = "Moeda"
+        cellFavorites?.labelSiglaBitcoin.text = "sigla"
+        cellFavorites?.labelPriceBitcoin.text = "R$ 31,000,00"
+        // cellFavorites?.imageIcon.image = ""
         return cellFavorites!
-
+    }
+    // MARK: - Layout da collection
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let larguraCell = collectionView.bounds.width / 2
+        return CGSize(width: larguraCell-15, height: 160)
     }
  }
